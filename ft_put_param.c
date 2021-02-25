b0dd4b36bf07a852cb4acf12e776cf92c9767dc2
Paul <paul@debian.paul>
2021-02-25 19:30:10 +0100

    add corrections

--- ft_put_param.c
+++ ft_put_param.c
@@ -6,7 +6,7 @@
 /*   By: pbrochar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 15:45:54 by pbrochar          #+#    #+#             */
-/*   Updated: 2021/02/25 19:21:28 by pbrochar         ###   ########.fr       */
+/*   Updated: 2021/02/25 19:30:00 by pbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ static void		init_param(s_varg *ftpf, s_flags *flags)
 
 	if (ftpf->str[ftpf->pos] == '0')
 		flags->nb_zero = count_flags(ftpf);
-	else if (ftpf->str[ftpf->pos] == '-' || ft_isdigit(ftpf->str[ftpf->pos]))
+	else if (ft_strchr("-*", ftpf->str[ftpf->pos]) || ft_isdigit(ftpf->str[ftpf->pos]))
 	{
 		(ftpf->pos)--;
 		nb = count_flags(ftpf);
