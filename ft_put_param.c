ab8652b32907fb5c7f8eddccc354c5e5bf0d12a7
Paul <paul@debian.paul>
2021-02-25 20:40:55 +0100

    add corrections

--- ft_put_param.c
+++ ft_put_param.c
@@ -6,7 +6,7 @@
 /*   By: pbrochar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 15:45:54 by pbrochar          #+#    #+#             */
-/*   Updated: 2021/02/25 20:25:53 by pbrochar         ###   ########.fr       */
+/*   Updated: 2021/02/25 20:40:02 by pbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,9 +45,9 @@ static void		init_param(s_varg *ftpf, s_flags *flags)
 
 	if (ftpf->str[ftpf->pos] == '0')
 		flags->nb_zero = count_flags(ftpf);
-	else if (ft_strchr("-*", ftpf->str[ftpf->pos]) || ft_isdigit(ftpf->str[ftpf->pos]))
+	else if (ft_strchr("-", ftpf->str[ftpf->pos]) || ft_isdigit(ftpf->str[ftpf->pos]))
 	{
-		(ftpf->pos)--;
+		//(ftpf->pos)--;
 		nb = count_flags(ftpf);
 		if (nb < 0)
 			flags->nb_dash = nb * -1;
@@ -66,7 +66,6 @@ void			ft_put_param(s_varg *ftpf)
 
 	init_s_flags(&flags);
 	ftpf->pos++;
-//	printf("preci = %d, dash = %d\n", flags.precision, flags.nb_dash);
 	init_param(ftpf, &flags);
 	print_flags(ftpf, &flags);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pbrochar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 15:26:26 by pbrochar          #+#    #+#             */
-/*   Updated: 2021/02/25 20:26:16 by pbrochar         ###   ########.fr       */
+/*   Updated: 2021/02/25 20:40:32 by pbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int main(void)
 {
 
 
-	printf("%d = ",	ft_printf("%3s%3s", "hello", "world"));
-//	printf("print pf: %d ==> ", printf("|%-*.98u\n|", 117,4198534266u));
-//	printf("print ft: %d ==> ", ft_printf("|%-*.98u\n|" ,117,4198534266u));
+//	printf("%d = ",	ft_printf("%3s%3s", "hello", "world"));
+	printf("|%-*.98u\n|", 117,4198534266u);
+	ft_printf("|%-*.98u\n|" ,117,4198534266u);
 //	printf("%s\n", NULL);
 
 	return (0);
