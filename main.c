7cd571bc137356e214b3896b00edef328371cbb2
Paul <paul@debian.paul>
2021-02-25 20:05:16 +0100

    add corrections

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pbrochar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 15:26:26 by pbrochar          #+#    #+#             */
-/*   Updated: 2021/02/25 19:52:49 by pbrochar         ###   ########.fr       */
+/*   Updated: 2021/02/25 20:02:55 by pbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int main(void)
 {
 
 
-	printf("print pf: %d ==> ", printf("|%.p|\n", NULL));
-	printf("print ft: %d ==> ", ft_printf("|%5.p|\n", NULL));
+	printf("print pf: %d ==> ", printf("|%026.51%|\n"));
+	printf("print ft: %d ==> ", ft_printf("|%026.51%|\n"));
 //	printf("%s\n", NULL);
 
 	return (0);
