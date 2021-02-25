f62a905d138586cb381b85c2140b47179645c3c5
Paul <paul@debian.paul>
2021-02-25 21:51:25 +0100

    add corrections

--- ft_put_param.c
+++ ft_put_param.c
@@ -6,7 +6,7 @@
 /*   By: pbrochar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 15:45:54 by pbrochar          #+#    #+#             */
-/*   Updated: 2021/02/25 21:41:25 by pbrochar         ###   ########.fr       */
+/*   Updated: 2021/02/25 21:48:05 by pbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,7 +90,7 @@ void			ft_put_param(s_varg *ftpf)
 	printf("zero = %d\n", flags.nb_zero);
 	printf("dash = %d\n", flags.nb_dash);
 	printf("space = %d\n", flags.nb_space);
-//	printf("precision = %d\n", flags.precision);
+	printf("precision = %d\n", flags.precision);
 //	printf("type = %c\n", flags.type);*/
 	print_flags(ftpf, &flags);
 }
