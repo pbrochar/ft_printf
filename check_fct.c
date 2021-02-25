695637b76ef59bf96af9d1eec189f1eae1f14717
Paul BROCHARD <pbrochar@e2r4p6.42.fr>
2021-02-25 16:03:04 +0100

    add file

--- check_fct.c
+++ check_fct.c
@@ -1,25 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   check_fct.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbrochar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/23 15:26:26 by pbrochar          #+#    #+#             */
-/*   Updated: 2021/02/25 16:00:44 by pbrochar         ###   ########.fr       */
+/*   Created: 2021/02/25 15:50:43 by pbrochar          #+#    #+#             */
+/*   Updated: 2021/02/25 16:02:53 by pbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include "ft_printf.h"
 
-int main(void)
+static int checl_format(s_varg *ftpf, int *i)
 {
+	s_flags flags;
 
+	(*i)++;
+	if (ftpf->str[*i] == '-' && ftpf->str[(*i) + 1] == '0')
+		return (-1);
+	else if (ftpf->str[*i] == '0' && ftpf->str[(*i) + 1] == '0')
+		return (-1);
+	init_s_flags(&flags);
+	if 
+}
 
-	printf("|%-010d|\n", 0);
-//	ft_printf("%s\n", NULL);
-//	printf("%s\n", NULL);
-
+int	check_fct(s_varg *ftpf)
+{
+	int i;
+	
+	i = 0;
+	while (ftpf->str[i])
+	{
+		if (ftpf->str[i] == '%')
+		{
+			if (check_format(ftpf, &i) == -1)
+				return (-1);
+		}
+		i++;
+	}
 	return (0);
 }
